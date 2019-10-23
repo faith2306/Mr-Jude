247dda27ba0eef90e95b4eba00fece53b6f3b50f
faith2306 <55436735+faith2306@users.noreply.github.com>
2019-10-22 21:29:23 -0700

    Add files via upload

---
+++ QuizOnBank.py
@@ -0,0 +1,25 @@
+
+class salary:
+      filename = ("data.txt","w+")
+      def readFile(self,filename):
+            self.fN = filename
+            open(self.file)
+
+
+      def __init__(self,Name,ID,Position,SalaryAmount):
+            self.name = Name
+            self.ID = str(ID)
+            self.position = Position
+            self.salaryAmt = SalaryAmount
+
+      def displayFile(self):
+            return (readFile().sort)
+
+input("wat do you want to do: \n login as newstaff enter 1 \n delete staff 2\n "
+      "view summary data enter 3\n save and exit 4")
+if input == "1":
+      input("enter ID number: ")
+      if input > 5 and input[0] != and input[1::] !=
+          print("ID first letter should be alphabelt,while the rest 4 "
+         "should be numbers ")
+
