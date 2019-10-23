6f0cd30885c7540b1c737db09705a2d0590c09d1
faith2306 <55436735+faith2306@users.noreply.github.com>
2019-10-23 00:49:08 -0700

    Update QuizOnBank.py

--- QuizOnBank.py
+++ QuizOnBank.py
@@ -15,11 +15,29 @@ class salary:
       def displayFile(self):
             return (readFile().sort)
 
-input("wat do you want to do: \n login as newstaff enter 1 \n delete staff 2\n "
+Newstaff = I1,I2,I3,I4,I5
+allInfo = [Newstaff]
+input("\nwat do you want to do: \n login as newstaff enter 1 \n delete staff 2\n "
       "view summary data enter 3\n save and exit 4")
 if input == "1":
-      input("enter ID number: ")
-      if input > 5 and input[0] != and input[1::] !=
-          print("ID first letter should be alphabelt,while the rest 4 "
-         "should be numbers ")
-
+    I1 = input("enter ID number: ")
+    if input > 5:
+            #and input[0] != and input[1::] !=
+        print("ID first letter should be alphabelt,while the rest 4 "
+          "should be numbers ")
+        continue
+        if I1 is in salaryApp print:
+            print('ID already exist choose another')
+            continue
+      #  else:
+       #     pass
+    I2 = input("enter name(not more than 19 words):")
+    if input > 20:
+        print("too much characters")
+         continue
+    I3 = input("enter input position either staff,officer,manager ")
+    SalaryRange = {'Staff': 3500000 – 7000000,'Officer': 7000001 – 10000000,'Manager': 10000000 "and above'}
+    I4 = input("enter your salary base on this range " SalaryRange.get(I3))
+elif input == "2":
+    input('input staff ID')
+    if input is in salaryApp
